ffbf266c71554b16ae33ef734bbd31e68de7e7bc
Gil Vieira <ogilvieira@gmail.com>
2016-03-29 15:29:38 -0300

    ajuste

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,8 @@
       xfbml      : true,
       version    : 'v2.5'
     });
-  };
-
-  FB.getLoginStatus(function(res){
-  	if(res.status == "conected"){
+	FB.getLoginStatus(function(res){
+		if(res.status == "conected"){
 		FB.api('/me', { fields: 'first_name,gender,hometown' }, function(res) {
 			if(res.error){ 
 				$('#result').removeClass('is-active').find('.result-wrap').html('');
@@ -22,31 +20,29 @@
 				uid : res.id
 			});
 		});  		
-  	};
-  });
-
+		};
+	});
+  };
 
   $('.btn-login').on('click', function(e){
   	e.preventDefault();
 
-  	
-  	
   	window.open("https://www.facebook.com/dialog/oauth?client_id="+DATA.fbAppId+"&redirect_uri=http://ogilvieira.github.io/planilha-odebrecht/", "_self");
 
   	//get fb login
 	// FB.login(function(res) {
-		FB.api('/me', { fields: 'first_name,gender,hometown' }, function(res) {
-			if(res.error){ 
-				$('#result').removeClass('is-active').find('.result-wrap').html('');
-				$('#intro').removeClass('is-inactive');
-				return; 
-			};
-			Generator.init({
-				first_name : res.first_name,
-				gender : res.gender,
-				uid : res.id
-			});
-		});
+		// FB.api('/me', { fields: 'first_name,gender,hometown' }, function(res) {
+		// 	if(res.error){ 
+		// 		$('#result').removeClass('is-active').find('.result-wrap').html('');
+		// 		$('#intro').removeClass('is-inactive');
+		// 		return; 
+		// 	};
+		// 	Generator.init({
+		// 		first_name : res.first_name,
+		// 		gender : res.gender,
+		// 		uid : res.id
+		// 	});
+		// });
 	// },{ scope: 'public_profile', auth_type: 'rerequest' });
   });
 
