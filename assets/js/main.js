adc12bb658cd8b0d6476de3c4ec46f2228acdfed
Gil Vieira <ogilvieira@gmail.com>
2016-03-29 09:12:33 -0300

    ajusta fb api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,8 @@
 ;(function($){
 
-
-// var getRandomInt = function (min, max) {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// }
-// var getFullName = function(name, gender){
-// var lastName = ['male', 'female'];
-// 	lastName['male'] = ['Colorido','Oculto','do Cachorrinho','Chato','Tranquilo','Diferentão','do Mangue','da Farinha','de SP','do Nacional','do Estadual','do Helicóptero','da Força','da Farsa','da Pérsia','Porcina','da Usabilidade','do Sítio','do Torresmo','de um Terço','Acarajé','Candomblé','Rei Pelé','Batoré','Reeleito','Campeão','do Segundo Turno','Grego','do Viagra','da Federal'];
-// 	lastName['female'] = ['Colorida','Oculta','do Cachorrinho','Chata','Tranquila','Diferentona','do Mangue','da Farinha','de SP','do Nacional','do Estadual','do Helicóptero','da Força','da Farsa','da Pérsia','Porcina','da Usabilidade','do Sítio','do Torresmo','de um Terço','Acarajé','Candomblé','Batoré','Reeleita','Campeã','do Segundo Turno','Grega','TPM','da Federal'];
-
-// 	return name+" "+lastName[gender][getRandomInt(0, (lastName[gender].length-1))];
-// };
   window.fbAsyncInit = function() {
     FB.init({
-      appId      : '1705492553024172',
+      appId      : DATA.fbAppId,
       xfbml      : true,
       version    : 'v2.5'
     });
