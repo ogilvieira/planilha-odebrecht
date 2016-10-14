ee67ca3adbde3eae209ea4459e800971768b0a7a
Gil Vieira <ogilvieira@users.noreply.github.com>
2016-10-14 11:22:56 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,7 @@
 
   $('.btn-login').on('click', function(e){
   	e.preventDefault();
-  	window.open("https://www.facebook.com/dialog/oauth?client_id="+DATA.fbAppId+"&redirect_uri=http://ogilvieira.github.io/planilha-odebrecht/", "_self");
+  	window.open("https://www.facebook.com/dialog/oauth?client_id="+DATA.fbAppId+"&redirect_uri=http://ogilvieira.com.br/planilha-odebrecht/", "_self");
   });
 
 })(jQuery || $);
